f28678195a620fe41ec6b9baba3ec89179f14660
Y3nny-C4tz1n <113565394+Y3nny-C4tz1n@users.noreply.github.com>
2022-09-29 10:53:35 -0500

    Add files via upload
    Creación loads atleta,entrenador,fomento, disciplina, asociación.

---
+++ application/views/loads/disciplinaModal.php
@@ -0,0 +1,9 @@
+<form id="form">
+  <input type="text" class="form-control" name="pk_disciplina" value="<?=$pk_disciplina?>" id="pk_disciplina" hidden>
+  <div class="row">
+    <div class="col-md-12">
+      <label for="nombre_disciplina" class="col-form-label">Nombre Disciplina:</label>
+      <input type="text" class="form-control" name="nombre_disciplina" id="nombre_disciplina" value="<?=$INFO['nombre_disciplina']?>" autocomplete="off" required>
+    </div>
+  </div>
+</form>
---
+++ application/views/loads/modalidadModal.php
@@ -0,0 +1,9 @@
+<form id="form">
+  <input type="text" class="form-control" name="pk_modalidad" value="<?=$pk_modalidad?>" id="pk_modalidad" hidden>
+  <div class="row">
+    <div class="col-md-12">
+      <label for="nombre_modalidad" class="col-form-label">Nombre Modalidad:</label>
+      <input type="text" class="form-control" name="nombre_modalidad" id="nombre_modalidad" value="<?=$INFO['nombre_modalidad']?>" autocomplete="off" required>
+    </div>
+  </div>
+</form>
