b9ccde40b3718b23182a9a7323e8bb8fe197df34
Y3nny-C4tz1n <113565394+Y3nny-C4tz1n@users.noreply.github.com>
2022-10-01 06:55:43 -0500

    Delete asociacionModal.php

--- application/views/loads/asociacionModal.php
+++
@@ -1,9 +0,0 @@
-<form id="form">
-  <input type="text" class="form-control" name="pk_asociacion" value="<?=$pk_asociacion?>" id="pk_asociacion" hidden>
-  <div class="row">
-    <div class="col-md-12">
-      <label for="nombre_asociacion" class="col-form-label">Nombre Asociación:</label>
-      <input type="text" class="form-control" name="nombre_asociacion" id="nombre_asociacion" value="<?=$INFO['nombre_asociacion']?>" autocomplete="off" required>
-    </div>
-  </div>
-</form>
