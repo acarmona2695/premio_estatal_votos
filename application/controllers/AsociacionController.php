ab6ed30bde674e0d40720c219927139fc4a4012f
Y3nny-C4tz1n <113565394+Y3nny-C4tz1n@users.noreply.github.com>
2022-10-01 07:00:41 -0500

    Add files via upload

---
+++ application/controllers/AsociacionController.php
@@ -0,0 +1,82 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+class AsociacionController extends My_Controller {
+	public function __construct(){
+		parent::__construct();
+		$this->load->model('AsociacionModel');
+		$this->load->model('CatalogoModel');
+	}
+	public function index(){
+		if($this->session->userdata('pb_idPerfil') != 1){
+			redirect(base_url().'asociacion');
+		}
+		$datos = $this->funcionesBasicas('Asociacion');
+		$datos['menu'] = "asociacion";
+		$this->load->view('asociacion',$datos);
+	}
+	public function listAsociacion(){
+		$filtros = array('tipo' => 'TOTAL', 'start' => '0', 'limit' => '0');
+		$count = $this->AsociacionModel->AsociacionModelo($filtros);
+		$tmp = $this->input->post('page');
+		$page = (!empty($tmp))?$tmp:1;
+		$tmp = $this->input->post('rows');
+		$limit = $filtros['limit'] = (!empty($tmp))?$tmp:10;
+		$start = $limit * $page - $limit;
+		$start = $filtros['start'] = ($start < 0) ? 0 : $start;
+		$filtros['tipo'] = 'PARCIAL';
+		$sidx = (isset($_POST['sidx']) && $_POST['sidx'] != "") ? $_POST['sidx'] : 'pk_asociacion';
+		$sord = (isset($_POST['sidx']) && $_POST['sidx'] != "") ? $_POST['sord'] : 'ASC';
+		$filtros['sidx'] = $sidx;
+		$filtros['sord'] = $sord;
+		$dataControl = $this->AsociacionModel->AsociacionModelo($filtros);
+		if($count > 0){
+			$total_pages = ceil($count/$limit);
+		}else{
+			$total_pages = 0;
+		}
+		if ($page > $total_pages){
+			$page=$total_pages;
+		}
+		$response = new stdClass();
+		$response->page = $page;
+		$response->total = $total_pages;
+		$response->records = $count;
+		if(count($dataControl) == 0){
+			echo json_encode(array());
+			die();
+		}
+		$i=0;
+		foreach($dataControl as $row){
+			$response->rows[$i]['id'] = $row['pk_asociacion'];
+			$response->rows[$i]['cell'] = array('pk_asociacion' => $row['pk_asociacion'],
+				"btnEditar" => '<button type="button" class="btn btn-labeled btn-primary btn-sm btnEditar" data-bs-toggle="modal" data-bs-target="#modalInventario" data-id="'.$row['pk_asociacion'].'"><span class="btn-label"><i class="bi bi-pencil"></i></span>  Editar</button>',
+				"descripcion" => $row['descripcion'],
+				"estatus" => $row['estatus'],
+			);
+			$i++;
+		}
+		echo json_encode($response);die();
+	}
+	public function guardarAsociacion(){
+		if(!$this->session->userdata('pb_idUsuario')){
+			echo json_encode(array("error" => true,"msg" => 'Favor de actualizar la página, su sesión finalizó.'));die();
+		}
+		$descripcion = $this->input->post('descripcion');
+		if(trim($descripcion) == ""){
+			echo json_encode(array("error" => true,"msg" => 'El campo Asociación es obligatorio'));die();
+		}
+		
+		$res = $this->AsociacionModel->AsociacionGuardar($_POST);
+		echo json_encode($res);die();
+	}
+	public function loadFormulario(){
+		$data['pk_asociacion'] = $this->input->post('pk_asociacion');
+		$data['INFO']['descripcion'] = '';
+		$data['INFO']['estatus'] = '';
+		if(intval($data['pk_asociacion']) > 0){//Editar
+			$data['INFO'] = $this->AsociacionModel->obtenerRegistroPorId($data['pk_asociacion']);
+		}
+		
+		echo json_encode(array("error" => false,"HTML" => $this->load->view("loads/asociacionModal",$data,TRUE),"msg" => ''));die();
+	}
+}
---
+++ application/controllers/NominadoController.php
@@ -0,0 +1,113 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+class NominadoController extends MY_Controller {
+	public function __construct(){
+		parent::__construct();
+		$this->load->model('NominadoModel');
+		$this->load->model('RegistroModel');
+	}
+	public function index(){
+		if($this->session->userdata('pb_idPerfil') != 1){
+			redirect(base_url().'nominado');
+		}
+		$datos = $this->funcionesBasicas('Nominado');
+		$datos['menu'] = "nominado";
+		$datos['ASOCIACION'] = $this->RegistroModel->listAsociacion();
+		$datos['MODALIDAD'] = $this->RegistroModel->listModalidad();
+		$this->load->view('nominado',$datos);
+	}
+	public function listNominado(){
+		if(!$this->session->userdata('pb_idUsuario')){
+			echo json_encode(array());
+			die();
+		}
+		$filtros = array('tipo' => 'TOTAL', 'start' => '0', 'limit' => '0');
+		$filtros['LIKE'] = array("nombre" => trim($this->input->post("nombre")));
+		$filtros['WHERE'] = array("asociacion" => trim($this->input->post("asociacion")),
+									"modalidad" => trim($this->input->post("modalidad")));
+		$count = $this->NominadoModel->NominadoModelo($filtros);
+		$tmp = $this->input->post('page');
+		$page = (!empty($tmp))?$tmp:1;
+		$tmp = $this->input->post('rows');
+		$limit = $filtros['limit'] = (!empty($tmp))?$tmp:10;
+		$start = $limit * $page - $limit;
+		$start = $filtros['start'] = ($start < 0) ? 0 : $start;
+		$filtros['tipo'] = 'PARCIAL';
+		$sidx = (isset($_POST['sidx']) && $_POST['sidx'] != "") ? $_POST['sidx'] : 'pk_nominado';
+		$sord = (isset($_POST['sidx']) && $_POST['sidx'] != "") ? $_POST['sord'] : 'ASC';
+		$filtros['sidx'] = $sidx;
+		$filtros['sord'] = $sord;
+		$dataControl = $this->NominadoModel->NominadoModelo($filtros);
+		if($count > 0){
+			$total_pages = ceil($count/$limit);
+		}else{
+			$total_pages = 0;
+		}
+		if ($page > $total_pages){
+			$page=$total_pages;
+		}
+		$response = new stdClass();
+		$response->page = $page;
+		$response->total = $total_pages;
+		$response->records = $count;
+		if(count($dataControl) == 0){
+			echo json_encode(array());
+			die();
+		}
+		$i=0;
+		foreach($dataControl as $row){
+			$response->rows[$i]['id'] = $row['pk_nominado'];
+			$response->rows[$i]['cell'] = array('pk_nominado' => $row['pk_nominado'],
+				"btnEditar" => '<button type="button" class="btn btn-labeled btn-primary btn-sm btnEditar" data-bs-toggle="modal" data-bs-target="#modalInventario" data-id="'.$row['pk_nominado'].'"><span class="btn-label"><i class="bi bi-pencil"></i></span>  Editar</button>',
+				"asociacion" => $row['asociacion'],
+				"nombre_nominado" => $row['nombre_nominado'],
+				//"fk_usuario" => $row['fk_usuario'],
+				"fecha_creacion" => $row['fecha_creacion'],
+				"fecha_modificacion" => $row['fecha_modificacion'],
+				"modalidad" => $row['modalidad'],
+			);
+			$i++;
+		}
+		echo json_encode($response);die();
+	}
+	public function guardarNominado(){
+		if(!$this->session->userdata('pb_idUsuario')){
+			echo json_encode(array("error" => true,"msg" => 'Favor de actualizar la página, su sesión finzalizó.'));die();
+		}
+		$fk_asociacion = $this->input->post('fk_asociacion');
+		if(trim($fk_asociacion) == ""){
+			echo json_encode(array("error" => true,"msg" => 'El campo Perfil es obligatorio'));die();
+		}
+
+		$nombre_nominado = $this->input->post('nombre_nominado');
+		if(trim($nombre_nominado) == ""){
+			echo json_encode(array("error" => true,"msg" => 'El campo Nombre es obligatorio'));die();
+		}
+		
+		$fk_modalidad = $this->input->post('fk_modalidad');
+		if(trim($fk_modalidad) == ""){
+			echo json_encode(array("error" => true,"msg" => 'El campo Modalidad es obligatorio'));die();
+		}
+		
+		$res = $this->NominadoModel->NominadoGuardar($_POST);
+		echo json_encode($res);die();
+	}
+	public function loadFormulario(){
+		$data['pk_nominado'] = $this->input->post('pk_nominado');
+		$data['INFO']['nombre_nominado'] = '';
+		//$data['INFO']['fk_usuario'] = '';
+		//$data['INFO']['estatus'] = '';
+		$data['INFO']['fk_asociacion'] = '';
+		$data['INFO']['fk_modalidad'] = '';
+		if(intval($data['pk_nominado']) > 0){//Editar
+			$data['INFO'] = $this->NominadoModel->obtenerRegistroPorId($data['pk_nominado']);
+			
+		}
+		$data['ASOCIACION'] = $this->RegistroModel->listAsociacion();
+		$data['MODALIDAD'] = $this->RegistroModel->listModalidad();
+		//echo json_encode($data);die();
+		echo json_encode(array("error" => false,"HTML" => $this->load->view("loads/nominadoModal",$data,TRUE),"msg" => ''));die();
+
+	
+	}
+}
